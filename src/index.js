19858687ed67b38ce1d2e25b15a25f18d0bf18e5
ffnn <fenoheriniainat@gmail.com>
2020-09-30 10:53:53 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FileViewer from "react-file-viewer";
+
+import "./styles.css";
+
+const file = "./Case-Study-Shell.pdf";
+const type = "pdf";
+
+const onError = (e) => {
+  console.log(e, "error in file-viewer");
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React File Viewer Demo</h1>
+      <h2>Displaying file with extension {type}</h2>
+      <FileViewer fileType={type} filePath={file} onError={onError} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("fileviewer");
+ReactDOM.render(<App />, root);
